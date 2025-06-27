abd0ebe541e7942a6852e8a909b3d9db436a5887
Guido Salimbeni <guido.salimbeni@gmail.com>
2025-06-27 19:45:08 +0100

    data and metadata

---
+++ knowledge.py
@@ -0,0 +1,36 @@
+postcode_meta = '''
+Column_Header,Description
+PCD,UK Postcode (frozen for 2011)
+PCD_Source,The governmental organization responsible for supplying the postcode
+X_BNG,Postcode easting for British National Grid/OSGB 1936 (EPSG:27700)
+Y_BNG,Postcode northing for British National Grid/OSGB 1936 (EPSG:27700)
+X_IG,Postcode easting for Irish Grid/TM65 (EPSG:29902)
+Y_IG,Postocde northing for Irish Grid/TM65 (EPSG:29902)
+X_Latitude,Postcode easting for World Geodetic System/WGS84 (EPSG:4326) - Latitude value used for GPS co-ordinates
+Y_Longitude,Postcode northing for World Geodetic System/WGS84 (EPSG:4326) - Longitude value used for GPS co-ordinates
+Total_Persons,Usually resident persons in postcode
+Females,Usually resident females in postcode
+Males,Usually resident males in postcode
+Occupied_Households,Usually resident households in postcode
+LSOA_DZ_Code,2011 census Lower Super Output Area (LSOA) and Data Zone code
+LSOA_DZ_Name,2011 census Lower Super Output Area (LSOA) and Data Zone name
+OA_SA_Code,2011 census Output Areas (OAs) and Small Areas
+OAC_Supergroup_Code,Output Area Classification Supergroup code (2011)
+OAC_Supergroup_Name,Output Area Classification Supergroup name (2011)
+OAC_Group_Code,Output Area Classification Group code (2011)
+OAC_Goup_Name,Output Area Classification Group name (2011)
+OAC_Subgroup_Code,Output Area Classification Subgroup code (2011)
+OAC_Subgroup_Name,Output Area Classification Subgroup name (2011)
+WIMD_2014_Rank,Welsh WIMD 2014 rank (based on 2011 LSOA)
+EIMD_2015_Score,English Index of Multiple Deprivation (EIMD) 2015 score (based on 2011 LSOAs)
+EIMD_2015_Rank,English Index of Multiple Deprivation (EIMD) 2015 rank (based on 2011 LSOAs)
+EIMD_2015_Decile,English Index of Multiple Deprivation (EIMD) 2015 decile (based on 2011 LSOAs)
+SIMD_2016_Rank,Scottish Index of Multiple Deprivation (SIMD) 2016 rank (based on 2011 Data Zones)
+NIMDM_2017_Rank,Northern Irish Multiple Deprivation Measurements (NIMDM) 2017 rank (based on 2011 SOAs)
+LSOA_DZ_Townsend_Deprivation_Score,Townsend Deprivation Index score of Lower Super Output Are (LSOA) and Data Zone
+LSOA_DZ_Townsend_Deprivation_Quintile,Townsend Deprivation Index quintile of Lower Super Output Are (LSOA) and Data Zone
+OA_SA_Townsend_Deprivation_Score,Townsend Deprivation Index score of Output Area (OA) and Small Area
+OA_SA_Townsend_Deprivation_Quintile,Townsend Deprivation Index quintile of Output Area (OA) and Small Area
+'''
+
+
